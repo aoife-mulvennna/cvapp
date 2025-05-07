3d9ab65d066d7a8106a2b4d7f6708ef0cb29de82
aoife <amulvenna10@qub.ac.uk>
2025-05-07 23:54:51 +0100

    implemented mui and theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+import theme from './theme';
+import { ThemeProvider, useTheme } from '@mui/material/styles';
 function App() {
+ 
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+    <div className="App" style={{ backgroundColor: theme.palette.main }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
+        <p sx={{color: theme.palette.light}}>
+        Aoife Mulvenna - Software Engineer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
+        <Button variant="contained" sx={{
+            backgroundColor: theme.palette.dark,
+            color: theme.palette.light,
+            '&:hover': {
+              backgroundColor: theme.palette.contrastText, 
+              spacing: theme.layout.spacing,
+            },
+          }}>About Me</Button>
+        <Button variant="contained" sx={{
+            backgroundColor: theme.palette.dark,
+            color: theme.palette.light,
+            '&:hover': {
+              backgroundColor: theme.palette.contrastText, 
+            },
+          }}>Education</Button>
+        <Button variant="contained" sx={{
+            backgroundColor: theme.palette.dark,
+            color: theme.palette.light,
+            '&:hover': {
+              backgroundColor: theme.palette.contrastText, 
+            },
+          }}>Experience</Button>
     </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App; 
+
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+  
+      main: '#FEBCC7',
+      light: '#FFFAFB',
+      dark: '#FE67A6',
+      contrastText: '#75005B',
+  },
+  layout:{
+    spacing: 4,
+  }
+});
+
+export default theme;
